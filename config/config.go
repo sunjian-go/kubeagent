5a6647e8ef5cff51b43db207cac06a6c58c8b54f
sunjian-go <18206389327@163.com>
2024-01-24 09:23:55 +0800

    2024/1/24提交：新增部署模式选择

--- config/config.go
+++ config/config.go
@@ -1,9 +1,7 @@
 package config
 
 const (
-	//Kubeconfig = "E:\\Code\\project\\kubernetes\\kubecp\\k8sconf\\conf.txt"
-	Kubeconfig     = "/root/.kube/config"
+	Kubeconfig = "E:\\Code\\project\\kubernetes\\kubecp\\k8sconf\\conf.txt" //本地
+	//Kubeconfig     = "/root/.kube/config"   //正式环境
 	PodLogTailLine = 2000 //tail的日志行数 tail -n 2000
-	//AdminUser      = "admin"
-	//AdminPasswd    = "admin123"
 )
--- service/init.go
+++ service/init.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"main/config"
 )
 
 var K8s k8s
@@ -15,18 +17,32 @@ type k8s struct {
 
 func (k *k8s) K8sInit() error {
 	// 配置 Kubernetes 客户端
-	//config, err := clientcmd.BuildConfigFromFlags("", config.Kubeconfig)
-	config, err := rest.InClusterConfig() //根据集群中的环境变量和配置文件创建一个 *rest.Config 对象，该对象包含与集群通信所需的认证信息和服务器地址等配置。
+	gconf, err := Conf.ReadConfFunc("server")
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+	//根据模式选择k8s的初始化方式
+	switch gconf["model"] {
+	case "docker":
+		k.Conf, err = clientcmd.BuildConfigFromFlags("", config.Kubeconfig) //读取config的方式
+		break
+	case "k8s":
+		//rest.InClusterConfig() 函数的作用是获取 Kubernetes 集群的配置信息，以便与集群进行交互。
+		//它通过检查环境变量来确定当前程序是否在 Kubernetes 集群内部运行。如果在集群内部运行，它将返回集群的配置信息，
+		//包括 API 服务器的地址和证书等。如果不在集群内部运行，它将返回错误。
+		k.Conf, err = rest.InClusterConfig() //部署的时候只需给工作负载绑定一个具有集群最高权限的sa即可
+		break
+	}
 
 	if err != nil {
 		return fmt.Errorf("获取 Kubernetes 配置失败: %v", err)
 	}
 
-	clientset, err := kubernetes.NewForConfig(config)
+	clientset, err := kubernetes.NewForConfig(k.Conf)
 	if err != nil {
 		return fmt.Errorf("创建 Kubernetes 客户端失败: %v", err)
 	}
 	k.Clientset = clientset
-	k.Conf = config
 	return nil
 }
